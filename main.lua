45d406b554503bc2e79bac3ce6c9be647414b8b9
Spectrum <zachgulliver@icloud.com>
2025-07-17 18:53:32 +0100

    Graphical changes

--- main.lua
+++ main.lua
@@ -1,7 +1,8 @@
 local directory = io.popen('dir "toConvert" /b')
 
-print("What file extension would you like to convert to? (e.g : jpg, png, cur, txt, py)")
+print("What file extension would you like to convert to? (e.g : jpg, png, cur, txt, py)\n")
 local target_Ext = io.read()
+print()
 
 local _, b = pcall(function()
     for file in directory:lines() do
@@ -12,9 +13,9 @@ local _, b = pcall(function()
     if base then
         local new_file = base .. "." .. target_Ext
 
-        print("Converted " .. file .. " to " .. new_file .. "!")
-
         os.rename("toConvert\\" .. file, "toConvert\\" .. new_file)
+
+        print("Converted " .. file .. " to " .. new_file .. "!\n\n")
     end
 
     ::continue::
