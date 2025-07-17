9be88318e117b29f3b823a44ef24931accf5872e
Spectrum <zachgulliver@icloud.com>
2025-07-17 11:11:23 +0100

    Code fixes

--- main.lua
+++ main.lua
@@ -1,25 +1,24 @@
-local directory = io.popen('dir "test" /b')
+local directory = io.popen('dir "toConvert" /b')
 
-print("What file extension would you like to convert to? (TYPE EXACTLY)")
+print("What file extension would you like to convert to? (e.g : jpg, png, cur, txt, py)")
 local target_Ext = io.read()
 
 local _, b = pcall(function()
     for file in directory:lines() do
     if file  == "hi.txt" then goto continue end
 
-    local base = file:match("(.+)%.%w+$")
+    local base = file:match("^(.*)%.([^%.]+)$")
 
     if base then
         local new_file = base .. "." .. target_Ext
 
         print("Converted " .. file .. " to " .. new_file .. "!")
 
-        os.rename("test\\" .. file, "test\\" .. new_file)
+        os.rename("toConvert\\" .. file, "toConvert\\" .. new_file)
     end
 
     ::continue::
     end
 end)
-
 
 if b then print(b) end
