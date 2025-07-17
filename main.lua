543c8a8803ef3ace442a67569db8ee64c2634bcb
Spectrum <zachgulliver@icloud.com>
2025-07-17 11:40:43 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,19 +5,19 @@ local target_Ext = io.read()
 
 local _, b = pcall(function()
     for file in directory:lines() do
-    if file  == "hi.txt" then goto continue end
+        if file  == "hi.txt" then goto continue end
 
-    local base = file:match("^(.*)%.([^%.]+)$")
+        local base = file:match("^(.*)%.([^%.]+)$")
 
-    if base then
-        local new_file = base .. "." .. target_Ext
+        if base then
+            local new_file = base .. "." .. target_Ext
 
-        print("Converted " .. file .. " to " .. new_file .. "!")
+            print("Converted " .. file .. " to " .. new_file .. "!")
 
-        os.rename("toConvert\\" .. file, "toConvert\\" .. new_file)
-    end
+            os.rename("toConvert\\" .. file, "toConvert\\" .. new_file)
+        end
 
-    ::continue::
+        ::continue::
     end
 end)
 
